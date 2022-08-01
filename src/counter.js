33717839c0fcda041dcc8661a80534e710f98b3d
Yasin <yasinm289@gmail.com>
2022-08-01 19:53:49 +0430

    counter app done

--- src/counter.js
+++ src/counter.js
@@ -2,21 +2,23 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
 
-    constructor(){
-        super();
-        this.state = { counter: 0 }; 
-    }
+    state = { counter: 0 }; 
    
     handleIncrement = () => {
         let counter = this.state.counter;
         counter++;
         this.setState({counter});
     };
+
     render() { 
         return (
            <div>
-            <h1>{this.state.counter}</h1>
-            <button onClick={this.handleIncrement}>+ Add</button>
+            <p className='mb-5'>
+                You pushed 
+                <span className='text-5xl font-bold'>{this.state.counter}</span>
+                 times
+            </p>
+            <button className='bg-blue-600 py-2 px-4 rounded text-sm hover:bg-blue-500' onClick={this.handleIncrement}>+ Add</button>
            </div>
         );
     }
